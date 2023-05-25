6514082af2514547316d50b291796a066420e3a8
WyBoii <132539622+WyBoii@users.noreply.github.com>
2023-05-25 11:01:03 +0400

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,84 @@
+<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+
+		header {
+			background-color: #333;
+			color: #fff;
+			padding: 20px;
+		}
+
+		header h1 {
+			margin: 0;
+		}
+
+		main {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+		}
+
+		section {
+			margin-bottom: 40px;
+		}
+
+		h2 {
+			margin-top: 0;
+		}
+
+		p {
+			line-height: 1.5;
+			margin-bottom: 20px;
+		}
+
+		img {
+			max-width: 100%;
+			height: auto;
+			margin-bottom: 20px;
+		}
+
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+		}
+
+		li {
+			margin-bottom: 10px;
+		}
+
+		li:before {
+			content: "•";
+			margin-right: 10px;
+			color: #333;
+		}
+
+		footer {
+			background-color: #333;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+		}
+	</style>
+		<h1>Обо мне</h1>
+		<section>
+			<h2>Вступление</h2>
+			<p>Привет, меня зовут Чмыхов Богдан, Я такой невероятно крутой, что слова не могут передать всей мощи моей крутости! Моя крутость настолько безгранична, что она просто выходит за рамки воображения! Я крутой, крутейший, невероятно крутой, просто божественный! Моя крутость такая, что даже самые крутые люди могут только мечтать о том, чтобы быть такими же крутым.</p>
+		</section>
+		<section>
+			<h2>Навыки:</h2>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>JavaScript</li>
+				<li>Node.JS</li>
+				<li>SveltaKit</li>
+			</ul>
+		</section>
+		<section>
+			<h2>Опыт</h2>
+			<p>Я рабботал там, потом тут, ну ещё вот там.</p>
+		</section>
